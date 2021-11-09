4ab69639a36bd6a8ca57e9108ce27f00f9f8d84d
Neil Tarar <aniltarar@gmail.com>
2021-11-10 09:51:56 +1100

    styling done

--- App/dad-joke-generator/src/componants/heading.jsx
+++ App/dad-joke-generator/src/componants/heading.jsx
@@ -41,58 +41,67 @@ class main extends Component {
     render() {
         return (
             <React.Fragment>
-                <div class="d-sm-inline-flex flex-column bd-highlight mb-3">
-                    <h1>Dad Joke Generator</h1>
-                    <form onSubmit={this.formSubmit}>
-                        <div className="radio">
-                            <label>
-                                <input
-                                    type="radio"
-                                    value="Programming"
-                                    checked={this.state.selectedOption === "Programming"}
-                                    onChange={this.onValueChange}
-                                />
-                                Programming
-                            </label>
-                        </div>
-                        <div className="radio">
-                            <label>
-                                <input
-                                    type="radio"
-                                    value="Pun"
-                                    checked={this.state.selectedOption === "Pun"}
-                                    onChange={this.onValueChange}
-                                />
-                                Pun
-                            </label>
-                        </div>
-                        <div className="radio">
-                            <label>
-                                <input
-                                    type="radio"
-                                    value="Spooky"
-                                    checked={this.state.selectedOption === "Spooky"}
-                                    onChange={this.onValueChange}
-                                />
-                                Spooky
-                            </label>
-                        </div>
-                        <div className="radio">
-                            <label>
-                                <input
-                                    type="radio"
-                                    value="Miscellaneous"
-                                    checked={this.state.selectedOption === "Miscellaneous"}
-                                    onChange={this.onValueChange}
-                                />
-                                Miscellaneous
-                            </label>
-                        </div>
-                        <button class="btn btn-success" type="submit">
-                            I Feel (un)Lucky
-                        </button>
-                    </form>
-                    <img src={mainImage} alt="" />
+                <div class="rounded-3 border border-4 border-success d-sm-inline-flex flex-column bd-highlight mb-3">
+                    <div className="p-2 bd-highlight">
+                        <h1 className="text-success font-monospace fw-bolder">Dad Joke Generator</h1>
+                    </div>
+                    <div className="p-2 bd-highlight">
+                        <form onSubmit={this.formSubmit}>
+                            <div className="radio">
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="Programming"
+                                        checked={this.state.selectedOption === "Programming"}
+                                        onChange={this.onValueChange}
+                                    />
+                                    Programming
+                                </label>
+                            </div>
+                            <div className="radio">
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="Pun"
+                                        checked={this.state.selectedOption === "Pun"}
+                                        onChange={this.onValueChange}
+                                    />
+                                    Pun
+                                </label>
+                            </div>
+                            <div className="radio">
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="Spooky"
+                                        checked={this.state.selectedOption === "Spooky"}
+                                        onChange={this.onValueChange}
+                                    />
+                                    Spooky
+                                </label>
+                            </div>
+                            <div className="radio">
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="Miscellaneous"
+                                        checked={this.state.selectedOption === "Miscellaneous"}
+                                        onChange={this.onValueChange}
+                                    />
+                                    Miscellaneous
+                                </label>
+                            </div>
+                            <br />
+                            <button className="btn btn-lg btn-outline-success" type="submit">
+                                I Feel (un)Lucky
+                            </button>
+                            <br />
+                        </form>
+                    </div>
+                    <div className="w-auto p-2 bd-highlight">
+                        <img src={mainImage} alt="" />
+                    </div>
+                    <br />
                     <div>
                         {this.state.joke}
                     </div>
